1d0a9dd0da1b17deb22176baab9de8e9b682afa2
Zemichael <yetikimitabeba@gmail.com>
2023-08-31 02:17:42 +0300

    Header file update.

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,16 +12,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
 /* Basic Binary Tree */
-typedef struct binary_tree_s binary_tree_t;
+/* typedef struct binary_tree_s binary_tree_t; */
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
