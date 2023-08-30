5d235bb44108bd6588488a278ba9151f506f085b
Zemichael <yetikimitabeba@gmail.com>
2023-08-31 00:54:23 +0300

    Header file update.

--- binary_trees.h
+++ binary_trees.h
@@ -17,8 +17,21 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+/* Basic Binary Tree */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/* Prototypes */
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
