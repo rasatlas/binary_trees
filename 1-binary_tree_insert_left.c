6d8b5116d19a2ddd0d83d50ac9fabcb46733acc8
Zemichael <yetikimitabeba@gmail.com>
2023-08-31 06:00:14 +0300

    1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,11 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (!parent)
-		return (NULL);
-
 	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (node)
+	if (node && parent)
 	{
 		node->n = value;
 		node->parent = parent;
