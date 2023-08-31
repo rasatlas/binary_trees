810f7813cfb7719aacf9d08f50378e78e5963d02
Zemichael <yetikimitabeba@gmail.com>
2023-09-01 01:53:14 +0300

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t depth = 0;
-		depth = 1 + binary_tree_depth(tree->parent);
+		depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 		return (depth);
 	}
 	return (0);
